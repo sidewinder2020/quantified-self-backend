0377bef5211503824ed6bdb1c5507a91a94ff087
sidewinder2020 <sdwinder1@gmail.com>
2018-02-05 14:13:37 -0700

    setup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var foods = require('./routes/foods')
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/api/v1/foods', foods);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
