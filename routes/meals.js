10cc1a5a936f564211ecfc73828b8c87cb7f5217
sidewinder2020 <sdwinder1@gmail.com>
2018-02-05 22:04:53 -0700

    get all foods, in meal object

--- routes/meals.js
+++ routes/meals.js
@@ -18,4 +18,21 @@ router.get('/', function(req, res, next) {
   })
 })
 
+router.get('/:id/foods', function(req, res, next) {
+  var mealId = req.params.id
+
+  var mealquery = database.raw('SELECT meals.id, meals.name from meals WHERE id=?',[mealId])
+  var foodquery = database.raw('SELECT foods.id, foods.name,foods.calories from foods join mealsfoods on foods.id = mealsfoods.food_id join meals on meals.id = mealsfoods.meal_id where meals.id=?',[mealId])
+
+  Promise.all([mealquery, foodquery]).then(function(mealfoodsarray) {
+    if(!mealfoodsarray[0].rows) {
+      return res.sendStatus(404)
+    } else {
+      var meal = mealfoodsarray[0].rows[0]
+      meal["foods"] = mealfoodsarray[1].rows
+      res.json(meal)
+    }
+  })
+})
+
 module.exports = router;
