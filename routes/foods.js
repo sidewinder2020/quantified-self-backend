716fedca01b1ca6740e55bd7a2dd415258ce4f76
Adrian Lara <adrianblara@gmail.com>
2018-02-06 01:12:52 -0700

    food patch API endpoint written

--- routes/foods.js
+++ routes/foods.js
@@ -4,11 +4,23 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+router.patch('/:id', function(req, res, next) {
+  let id = req.params.id
+  let updatedInfo = req.body.food
 
+  if (!updatedInfo) {
+    return res.send(422).send({
+      error: "No parameters provided."
+    })
+  }
 
-
-
-
-
+  database('foods').where('id', id).update(updatedInfo)
+    .then(function(success) {
+      database('foods').where('id', id)
+      .then(function(food) {
+        res.status(201).json(food)
+      })
+    })
+})
 
 module.exports = router;
