347620a96fe3c6ac85b1ad10d793314583b71b4c
Adrian Lara <adrianblara@gmail.com>
2018-02-07 16:10:29 -0700

    Delete food route created

--- routes/foods.js
+++ routes/foods.js
@@ -4,11 +4,17 @@ const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../knexfile')[environment]
 const database = require('knex')(configuration)
 
+router.delete('/:id', function(req, res, next) {
+  let id = req.params.id
 
-
-
-
-
-
+  return database('foods').where({ id: id }).del()
+    .then(function(result) {
+      if (result === 1) {
+        return res.sendStatus(204)
+      } else {
+        return res.sendStatus(404)
+      }
+    })
+})
 
 module.exports = router;
