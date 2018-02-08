da38556657b4b27a11032ef0ad616e12e02fe026
Adrian Lara <adrianblara@gmail.com>
2018-02-08 14:46:36 -0700

    delete meal food endpoint written

--- routes/meals.js
+++ routes/meals.js
@@ -35,4 +35,27 @@ router.get('/:id/foods', function(req, res, next) {
   })
 })
 
+router.delete('/:meal_id/foods/:id', function(req, res, next) {
+  let meal_id = req.params.meal_id
+  let food_id = req.params.id
+  let find_meal_name = database('foods').where({id: food_id})
+  let find_food_name = database('meals').where({id: meal_id})
+
+  Promise.all([
+    find_meal_name.then(function(result) {food_name = result[0].name}),
+    find_food_name.then(function(result) {meal_name = result[0].name})
+    ])
+      .then(function() {
+        database('mealsfoods').where({ meal_id: meal_id, food_id: food_id}).del()
+          .then(function(result) {
+            if (result === 1) {
+              res.send({ "message": `Successfully removed ${food_name} from ${meal_name}` })
+            } else {
+              res.sendStatus(404)
+            }
+          })
+      })
+})
+
+
 module.exports = router;
