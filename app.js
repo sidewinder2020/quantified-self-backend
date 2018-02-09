b708f381bab1c4ba9b7c1db2f37dc82f0d28fa55
Adrian Lara <adrianblara@gmail.com>
2018-02-08 22:09:02 -0700

    expost get, post, patch, and delete methods

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
